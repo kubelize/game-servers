27692d51822cd4a2fb0388990de730e231e2465a
Daniel Hendricken <daniel.hendricken@bedag.ch>
2025-02-04 23:34:44 +0100

    chore: working on serverconfig.template

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ sdtd
 When using a config copied into /home/steam/kubelize the server installs successfully but finally fails with the message: "Segmentation fault".
 
 -> Todo: Figure out what causes this
+
+Currently working on template in containers/game-servers/server-configs/seven_days_to_die/kubelize/serverconfig.template
--- containers/game-servers/server-configs/seven_days_to_die/entrypoint.sh
+++ containers/game-servers/server-configs/seven_days_to_die/entrypoint.sh
@@ -1,3 +1,27 @@
-#!/bin/bash
-/home/steam/steamcmd/steamcmd.sh +force_install_dir /home/steam/sdtd/ +login anonymous +app_update 294420 +quit
-/home/steam/sdtd/startserver.sh -configfile=/home/steam/kubelize/serverconfig.xml
+#!/bin/sh
+
+# Define paths to the template and final config
+TEMPLATE_PATH="/config/serverconfig.template"
+OUTPUT_PATH="/config//serverconfig.xml"
+
+# Load values directly from the ConfigMap and Secret into temporary files
+CONFIG_FILE="/config/config-values.yaml"
+PASSWORD_FILE="/config/ServerPassword"
+
+# Generate the configuration using dockerize
+echo "Generating configuration from template..."
+
+dockerize -template "$TEMPLATE_PATH:$OUTPUT_PATH" \
+  -template="$CONFIG_FILE:/config/config-values.yaml" \
+  -template="$PASSWORD_FILE:/config/ServerPassword"
+
+# Check if the config was created successfully
+if [ -f "$OUTPUT_PATH" ]; then
+  echo "Configuration generated successfully."
+else
+  echo "Failed to generate configuration."
+  exit 1
+fi
+
+# Start the server or application (replace with actual command)
+
