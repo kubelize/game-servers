12daafc51837e513774893e33702be315f292417
Daniel Hendricken <daniel.hendricken@bedag.ch>
2025-02-26 23:31:12 +0100

    chore: started working on ce

---
+++ containers/images/bin/conan_exiles/entrypoint.sh
@@ -0,0 +1,3 @@
+echo "Installing Seven Days to Die Dedicated Server"
+/home/kubelize/steam/steamcmd.sh +force_install_dir /home/kubelize/server +login anonymous +app_update 443030 validate +quit
+echo "Installation of Seven Days to Die Dedicated Server complete"
--- containers/images/build-containers.sh
+++ containers/images/build-containers.sh
@@ -1,7 +1,14 @@
 
-podman build --platform linux/amd64 -t kubelize/game-servers:0.2.1-ln -f Dockerfile.ln
-podman build --platform linux/amd64 -t kubelize/game-servers:0.2.0-we -f Dockerfile.we
-podman push kubelize/game-servers:0.2.1-ln
-podman push kubelize/game-servers:0.2.0-we
+# base
+podman build --platform linux/amd64 -t kubelize/game-servers:0.2.2-ln -f images/Dockerfile.ln
+podman build --platform linux/amd64 -t kubelize/game-servers:0.2.2-we -f images/Dockerfile.we
+podman push kubelize/game-servers:0.2.2-ln
+podman push kubelize/game-servers:0.2.2-we
+
+# sdtd
 podman build --platform linux/amd64 -t kubelize/game-servers:0.2.9-sdtd -f images/Dockerfile.sdtd
 podman push kubelize/game-servers:0.2.9-sdtd
+
+# conan exiles
+podman build --platform linux/amd64 -t kubelize/game-servers:0.1.0-ce -f images/Dockerfile.ce
+podman push kubelize/game-servers:0.1.0-ce
