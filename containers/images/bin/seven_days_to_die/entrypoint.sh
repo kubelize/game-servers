d875477af03fb4c04140d4ddac5c85d1db29e831
Daniel Hendricken <daniel.hendricken@bedag.ch>
2025-03-15 22:00:05 +0100

    chore: added logs for broken sdtd (its the configfile)

--- containers/images/bin/seven_days_to_die/entrypoint.sh
+++ containers/images/bin/seven_days_to_die/entrypoint.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-echo "Rendering serverconfig.xml"
-gomplate -f /home/kubelize/steam/config-data/serverconfig.template \
-         -d config=/home/kubelize/steam/config-data/config-values.yaml \
-         -d password=/home/kubelize/steam/config-data/serverpassword.yaml \
-         -o /home/kubelize/steam/config-data/serverconfig.xml
-echo "Rendering of the serverconfig complete"
 
-echo "Checking if the serverconfig already exists"
-if [ ! -f /home/kubelize/server/serverconfig.xml ]; then
-    echo "Moving the newly generated serverconfig.xml to server directory"
-    mv /home/kubelize/steam/config-data/serverconfig.xml /home/kubelize/server/
+echo "checking if the serverconfig already exists"
+if [ ! -f /home/kubelize/server/sdtdconfig.xml ]; then
+
+    echo "serverconfig.xml does not exist in server directory"
+
+    gomplate -f /home/kubelize/steam/config-data/serverconfig.template \
+            -d config=/home/kubelize/steam/config-data/config-values.yaml \
+            -d password=/home/kubelize/steam/config-data/serverpassword.yaml \
+            -o /home/kubelize/steam/config-data/serverconfig.xml
+
+    echo "Rendered serverconfig"
+    echo "adding the newly generated serverconfig.xml to server directory"
+
+    cp /home/kubelize/steam/config-data/serverconfig.xml /home/kubelize/server/sdtdconfig.xml 
+    chmod +x /home/kubelize/server/sdtdconfig.xml
+    echo "serverconfig.xml added to server directory"
 fi
 
-echo "Installing Seven Days to Die Dedicated Server"
+echo "installing the sdtd server"
 /home/kubelize/steam/steamcmd.sh +force_install_dir /home/kubelize/server +login anonymous +app_update 294420 validate +quit
-echo "Installation of Seven Days to Die Dedicated Server complete"
+echo "installation complete"
 
-echo "Starting Seven Days to Die Dedicated Server"
-/home/kubelize/server/startserver.sh -configfile=/home/kubelize/server/serverconfig.xml
+echo "starting sdtd server"
+/home/kubelize/server/startserver.sh -configfile=/home/kubelize/server/sdtdconfig.xml
