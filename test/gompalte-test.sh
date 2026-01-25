2347b94e33ba0ddf8d862124f5d79b77f32ae3d4
flyhigh-debug <daniel.i.hendricken@gmail.com>
2026-01-25 17:57:49 +0100

    chore(test): removed old files

--- test/gompalte-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gomplate -f /Users/dan/Git/kubelize/game-servers/test/serverconfig.template \
-         -d config=/Users/dan/Git/kubelize/game-servers/test/config-test.yaml \
-         -d password=/Users/dan/Git/kubelize/game-servers/test/serverpassword.yaml \
-         -o /Users/dan/Git/kubelize/game-servers/test/serverconfig.xml
